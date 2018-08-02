b979f55f0802d95dd255f31b130632834da0d9c4
Jhon Campo <jhon.campo@aspsols.com>
2018-08-02 17:47:54 -0500

    Prueba de commit nuevos archivos desde eclipse.

---
+++ JSP7 Cotizaciones/src/main/java/com/aspsols/cotizaciones/model/CotSeccionesSinonimosList.java
@@ -0,0 +1,17 @@
+package com.aspsols.cotizaciones.model;
+
+import java.util.List;
+
+public class CotSeccionesSinonimosList {
+
+	private List<CotSeccionesSinonimos> list;
+
+	public List<CotSeccionesSinonimos> getList() {
+		return list;
+	}
+
+	public void setList(List<CotSeccionesSinonimos> list) {
+		this.list = list;
+	}
+
+}
